e33c04acec0acd0aaea364e7d4633763770f4e1a
santiagoSuarez219 <santiago8628@gmail.com>
2023-08-12 13:06:16 -0500

    Creacion de modulos de la app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SensoresModule } from './sensores/sensores.module';
+import { ActuadoresModule } from './actuadores/actuadores.module';
+import { MqttModule } from './mqtt/mqtt.module';
+import { WebsocketModule } from './websocket/websocket.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [SensoresModule, ActuadoresModule, MqttModule, WebsocketModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
