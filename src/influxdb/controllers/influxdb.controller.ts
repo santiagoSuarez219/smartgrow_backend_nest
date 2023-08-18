8bd233e5040cb718ee98ae95bda3a0d296c75b97
santiagoSuarez219 <santiago8628@gmail.com>
2023-08-18 12:10:59 -0500

    Limpieza controlador influx db

--- src/influxdb/controllers/influxdb.controller.ts
+++ src/influxdb/controllers/influxdb.controller.ts
@@ -1,17 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { InfluxdbService } from '../services/influxdb.service';
 
 @Controller('influxdb')
 export class InfluxdbController {
   constructor(private readonly influxdbService: InfluxdbService) {}
-
-  @Get()
-  testConnectionInflux() {
-    return this.influxdbService.create_data_test();
-  }
-
-  @Get('query')
-  testQueryInflux() {
-    return this.influxdbService.query_test();
-  }
 }
