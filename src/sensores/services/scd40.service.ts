05fae41f5b8c155b83bf2f9fb78b6dc703eb1983
santiagoSuarez219 <santiago8628@gmail.com>
2023-09-23 15:33:36 +0000

    docker app and bug hour

--- src/sensores/services/scd40.service.ts
+++ src/sensores/services/scd40.service.ts
@@ -23,6 +23,7 @@ export class Scd40Service {
 
   create(data: CreateDocumentScd40Dto) {
     const date = new Date();
+    date.setHours(date.getHours() - 5)
     const newDocumentData = {
       ...data,
       fecha: date,
