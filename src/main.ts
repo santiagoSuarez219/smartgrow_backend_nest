0588ec18c37cf961a20fea250de443e87946fcf3
santiagoSuarez219 <santiago8628@gmail.com>
2023-10-21 17:09:32 -0500

    ValidationPipe Global

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   // Configuración Swagger en NestJS
   const config = new DocumentBuilder()
     .setTitle('smartgrow API')
